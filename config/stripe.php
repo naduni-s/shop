585ce137158328905efb59a34709f478eeb86a98
naduni-s <153343637+naduni-s@users.noreply.github.com>
2025-01-24 09:14:15 +0530

    Remove old files

--- config/stripe.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-    'stripe_pk' => env('STRIPE_TEST_PK'),
-    'stripe_sk' => env('STRIPE_TEST_SK'),
-];
--- tests/Feature/Auth/UserLoginTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-it('renders the login page', function () {
-    $response = $this->get('/login');
-
-    $response->assertStatus(200);
-    $response->assertSee('Login'); 
-});
